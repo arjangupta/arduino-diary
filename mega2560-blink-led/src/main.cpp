93d5719d6daa1f5572f795fda3dc4b793482f4f9
Arjan Gupta <arjangupta95@gmail.com>
2021-08-01 01:18:07 -0500

    Add double blink pattern

--- mega2560-blink-led/src/main.cpp
+++ mega2560-blink-led/src/main.cpp
@@ -32,8 +32,13 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  // Double blink pattern
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(200);                        // wait for 200 ms
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);                       // wait for a second
 }
