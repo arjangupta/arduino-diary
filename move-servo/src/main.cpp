9d316d08816ee8a32ff500be545c6fa8bab04e6b
Arjan Gupta <arjangupta95@gmail.com>
2022-11-09 22:16:30 -0600

    Comment out manual servo way and bring in Servo lib

--- move-servo/src/main.cpp
+++ move-servo/src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
 
-#define servoPin 9
+// MANUAL WAY
+// #define servoPin 8
 
-void setup() {
-  pinMode(servoPin, OUTPUT);
-}
+// void setup() {
+//   pinMode(servoPin, OUTPUT);
+// }
 
-void loop() {
-  // A pulse each 20 ms
-  digitalWrite(servoPin, HIGH);
-  delayMicroseconds(1450); // Duration of the pulse in microseconds
-  digitalWrite(servoPin, LOW);
-  delayMicroseconds(18550); // Remaining duration of the pulse in low (total should be 20ms)
-}
+// void loop() {
+//   // A pulse each 20 ms
+//   digitalWrite(servoPin, HIGH);
+//   // Pule durations: 600 - 0 deg; 1450 - 90 deg; 2300 - 180 deg
+//   delayMicroseconds(600); // Duration of the pulse in microseconds
+//   digitalWrite(servoPin, LOW);
+//   delayMicroseconds(18550); // Remaining duration of the pulse in low (total should be 20ms)
+// }
+
+#include <Servo.h>
