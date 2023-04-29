99ae73dda542529ac670bb71e1ed1c661817818e
Arjan Gupta <arjangupta95@gmail.com>
2023-04-29 00:36:19 -0500

    Set list of targets for joint3

--- 6dof-robot-pca9685/src/main.cpp
+++ 6dof-robot-pca9685/src/main.cpp
@@ -38,8 +38,12 @@ void setup() {
 }
 
 // Define list of random targets for joint 0, between 170 and 350
-uint16_t joint0_targets[5] = {170, 200, 250, 300, 350};
+uint16_t joint0_targets[5] = {300, 170, 250, 200, 350};
 uint8_t joint0_target_index = 0;
+
+// Define list of random targets for joint 3, between SERVOMIN and SERVOMAX
+uint16_t joint3_targets[5] = {SERVOMIN, 300, 250, 200, SERVOMAX};
+uint8_t joint3_target_index = 0;
 
 void loop() {
   // Propagate the joints
@@ -47,13 +51,23 @@ void loop() {
     joints[i].propagate();
   }
 
-  // Check if robot is done moving, then set new target
+  // Check if joint 0 is done moving, then set new target
   if (!joints[0].isMoving()) {
     Serial.println("Setting new target for joint0");
     joints[0].setTargetAngle(joint0_targets[joint0_target_index]);
     joint0_target_index++;
     if (joint0_target_index > 4) {
       joint0_target_index = 0;
+    }
+  }
+
+  // Check if joint 3 is done moving, then set new target
+  if (!joints[3].isMoving()) {
+    Serial.println("Setting new target for joint3");
+    joints[3].setTargetAngle(joint3_targets[joint3_target_index]);
+    joint3_target_index++;
+    if (joint3_target_index > 4) {
+      joint3_target_index = 0;
     }
   }
   
