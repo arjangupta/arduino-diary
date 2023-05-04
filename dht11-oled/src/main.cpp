e2fe360421139e64236a26ae752dfddfa2362333
Arjan Gupta <arjangupta95@gmail.com>
2023-05-04 00:11:18 -0500

    Add recreated oled proj

---
+++ dht11-oled/src/main.cpp
@@ -0,0 +1,79 @@
+#include <Arduino.h>
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+  dht.begin();
+}
+
+void i2cScanner() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+}
+
+void loop() {
+  // Delay between measurements
+  delay(10000);
+  // Read humidity
+  float humidity = dht.readHumidity();
+  // Read temperature as Fahrenheit
+  float temp_f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(humidity) || isnan(temp_f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(temp_f, humidity);
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temp_f);
+  Serial.print(" F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hif);
+  Serial.println(" F");
+
+  i2cScanner();
+}
