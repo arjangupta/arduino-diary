67b2c0d97ac5683e92ea5eb528feaa3c6fec1fb5
Arjan Gupta <arjangupta95@gmail.com>
2023-05-04 01:02:49 -0500

    Remove i2c scanner code

--- dht11-oled/src/main.cpp
+++ dht11-oled/src/main.cpp
@@ -13,42 +13,6 @@ void setup() {
   dht.begin();
 }
 
-void i2cScanner() {
-  byte error, address;
-  int nDevices;
-
-  Serial.println("Scanning...");
-
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-
-      nDevices++;
-    }
-    else if (error==4) {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  }
-  else {
-    Serial.println("done\n");
-  }
-}
-
 void loop() {
   // Delay between measurements
   delay(10000);
@@ -75,6 +39,4 @@ void loop() {
   Serial.print("Heat index: ");
   Serial.print(hif);
   Serial.println(" F");
-
-  i2cScanner();
 }
