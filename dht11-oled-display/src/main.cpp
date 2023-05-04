1e87eb462162a225d8eb7e5767fe05c1537971be
Arjan Gupta <arjangupta95@gmail.com>
2023-05-03 23:27:43 -0500

    Copy over DHT11 periodic code

--- dht11-oled-display/src/main.cpp
+++ dht11-oled-display/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Delay between measurements
+  delay(10000);
+  // Read humidity
+  float humidity = dht.readHumidity();
+  // Read temperature as Fahrenheit
+  float temp_f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(humidity) || isnan(temp_f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(temp_f, humidity);
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temp_f);
+  Serial.print(" F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hif);
+  Serial.println(" F");
 }
